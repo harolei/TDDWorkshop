70539a6614b60d0e252d33df456e0c48c039292c
leiyu <harolei@gmail.com>
2021-05-17 23:59:19 +0800

    FizzBuzz I2 refactor

--- app/src/main/java/online/study/tdd/FizzBuzzGame.java
+++ app/src/main/java/online/study/tdd/FizzBuzzGame.java
@@ -2,21 +2,24 @@ package online.study.tdd;
 
 public class FizzBuzzGame {
     public String handleInputNumber(int inputNumber) {
-        if (inputNumber % 3 == 0 && inputNumber % 5 == 0 && inputNumber % 7 == 0) {
-            return "fizzbuzzwhizz";
-        } else if (inputNumber % 3 == 0 && inputNumber % 5 == 0) {
-            return "fizzbuzz";
-        } else if (inputNumber % 3 == 0 && inputNumber % 7 == 0) {
-            return "fizzwhizz";
-        } else if (inputNumber % 5 == 0 && inputNumber % 7 == 0) {
-            return "buzzwhizz";
-        } else if (inputNumber % 3 == 0) {
-            return "fizz";
-        } else if (inputNumber % 5 == 0) {
-            return "buzz";
-        } else if (inputNumber % 7 == 0) {
-            return "whizz";
+        String result = getFizzBuzz(inputNumber);
+        if (result.equals("")) {
+            result = String.valueOf(inputNumber);
         }
-        return String.valueOf(inputNumber);
+        return result;
+    }
+
+    private String getFizzBuzz(int inputNumber) {
+        String result = "";
+        if (inputNumber % 3 == 0) {
+            result += "fizz";
+        }
+        if (inputNumber % 5 == 0) {
+            result += "buzz";
+        }
+        if (inputNumber % 7 == 0) {
+            result += "whizz";
+        }
+        return result;
     }
 }
