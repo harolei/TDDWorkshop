a30006ae973c78860dcc64f6d7cd220316b2f4d3
leiyu <harolei@gmail.com>
2021-05-18 11:09:45 +0800

    add assert examples

--- app/src/test/java/online/study/AppTest.java
+++ app/src/test/java/online/study/AppTest.java
@@ -4,11 +4,29 @@
 package online.study;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 public class AppTest {
-    @Test public void testAppHasAGreeting() {
+    @Test
+    public void testAppHasAGreeting() {
         App classUnderTest = new App();
         assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+    }
+
+    @Test
+    public void testSubstring() {
+        assertNull(null);
+        assertNotNull("abcd".substring(1));
+        assertFalse("abcd".toLowerCase().endsWith("q"));
+        assertTrue("abcd".toUpperCase().startsWith("A"));
+        assertEquals("abcd".toUpperCase(), "ABCD");
+        assertThat("abcd".substring(1, 2), is("b"));
     }
 }
